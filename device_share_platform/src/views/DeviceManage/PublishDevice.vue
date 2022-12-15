c9a04836fb5146f49cb3b170052c8ed407421007
adolfvitor <tong.shen@17dianjia.com>
2022-12-16 01:38:33 +0800

    update: form-ui

--- device_share_platform/src/views/DeviceManage/PublishDevice.vue
+++ device_share_platform/src/views/DeviceManage/PublishDevice.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
 import { ref, reactive, unref } from "vue";
-import { ElCascader, ElDivider, ElButton } from "element-plus";
+import { ElButton, ElUpload, ElTooltip } from "element-plus";
+import type { UploadProps, UploadUserFile } from "element-plus";
 import areaLists from "@/utils/area.json";
 import { ContentWrap } from "@/components/ContentWrap";
-import Form from "./components/Form.vue";
+import { Form } from "@/components/FormCommon";
 import { useValidator } from "@/hooks/web/useValidator";
 import { useForm } from "@/hooks/web/useForm";
 
@@ -62,6 +63,30 @@ const submitHandle = async () => {
     }
   });
 };
+/**
+ * upload start
+ */
+const fileList = ref<UploadUserFile[]>([
+  {
+    name: "food.jpeg",
+    url: "https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100",
+  },
+  {
+    name: "food2.jpeg",
+    url: "https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100",
+  },
+]);
+
+const handleRemove: UploadProps["onRemove"] = (uploadFile, uploadFiles) => {
+  console.log(uploadFile, uploadFiles);
+};
+
+const handlePreview: UploadProps["onPreview"] = (file) => {
+  console.log(file);
+};
+/**
+ * upload end
+ */
 
 const schema = reactive<FormSchema[]>([
   {
@@ -100,9 +125,52 @@ const schema = reactive<FormSchema[]>([
     },
   },
   {
+    field: "device_num",
+    component: "Input",
+    label: "设备编号：",
+    colProps: {
+      span: 10,
+    },
+    formItemProps: {
+      // rules: [required()],
+      style: {
+        // width: "100px",
+      },
+    },
+    componentProps: {
+      placeholder: "请输入设备编号",
+      inputStyle: {
+        width: "200px",
+      },
+    },
+  },
+  {
+    field: "device_type1",
+    component: "Input",
+    label: "设备型号：",
+    colProps: {
+      span: 10,
+    },
+    formItemProps: {
+      // rules: [required()],
+      style: {
+        // width: "100px",
+      },
+    },
+    componentProps: {
+      placeholder: "请输入设备型号",
+      inputStyle: {
+        width: "200px",
+      },
+    },
+  },
+  {
     field: "use_date",
     component: "DatePicker",
     label: "启用时间：",
+    colProps: {
+      span: 10,
+    },
     componentProps: {
       type: "date",
       placeholder: "请选择时间",
@@ -112,6 +180,359 @@ const schema = reactive<FormSchema[]>([
       //   width: "300px",
       // },
       // format:
+    },
+  },
+  {
+    field: "device_type2",
+    component: "Input",
+    label: "设备类型：",
+    colProps: {
+      span: 10,
+    },
+    formItemProps: {
+      rules: [required()],
+      style: {
+        // width: "100px",
+      },
+    },
+    componentProps: {
+      placeholder: "请输入设备类型",
+      inputStyle: {
+        width: "200px",
+      },
+    },
+  },
+  {
+    field: "project",
+    component: "Input",
+    label: "所在项目：",
+    colProps: {
+      span: 10,
+    },
+    formItemProps: {
+      // rules: [required()],
+      style: {
+        // width: "100px",
+      },
+    },
+    componentProps: {
+      placeholder: "请输入所在项目",
+      inputStyle: {
+        width: "200px",
+      },
+    },
+  },
+  {
+    field: "device_category",
+    label: "设备分类：",
+    component: "Radio",
+    colProps: {
+      span: 10,
+    },
+    formItemProps: {
+      rules: [required()],
+      style: {
+        // width: "100px",
+      },
+    },
+    componentProps: {
+      options: [
+        {
+          label: "大型机械",
+          value: "1",
+        },
+        {
+          label: "其它机械",
+          value: "2",
+        },
+      ],
+    },
+  },
+
+  {
+    field: "performance",
+    component: "Input",
+    label: "主要性能：",
+    colProps: {
+      span: 10,
+    },
+    formItemProps: {
+      // rules: [required()],
+      style: {
+        // width: "100px",
+      },
+    },
+    componentProps: {
+      placeholder: "请输入主要性能",
+      inputStyle: {
+        width: "200px",
+      },
+    },
+  },
+  {
+    field: "field41",
+    label: "",
+    component: "Divider",
+  },
+  {
+    field: "price1",
+    component: "Input",
+    label: "指导价格(元/月):",
+    colProps: {
+      span: 10,
+    },
+    formItemProps: {
+      // rules: [required()],
+      labelWidth: "124px",
+      style: {
+        // width: "100px",
+      },
+    },
+    componentProps: {
+      placeholder: "请输入指导价格",
+      type: "number",
+      clearable: false,
+      // type: 'textarea',
+      inputStyle: {
+        width: "200px",
+      },
+    },
+  },
+  {
+    field: "price2",
+    component: "Input",
+    label: "指导价格(元/台班):",
+    colProps: {
+      span: 10,
+    },
+    formItemProps: {
+      // rules: [required()],
+      labelWidth: "129px",
+      style: {
+        // width: "100px",
+      },
+    },
+    componentProps: {
+      placeholder: "请输入指导价格",
+      type: "number",
+      clearable: false,
+      // type: 'textarea',
+      inputStyle: {
+        width: "200px",
+      },
+    },
+  },
+  {
+    field: "price3",
+    component: "Input",
+    label: "设备原值(元):",
+    colProps: {
+      span: 10,
+    },
+    formItemProps: {
+      // rules: [required()],
+      labelWidth: "124px",
+      style: {
+        // width: "100px",
+      },
+    },
+    componentProps: {
+      placeholder: "请输入设备原值",
+      type: "number",
+      clearable: false,
+      // type: 'textarea',
+      inputStyle: {
+        width: "200px",
+      },
+    },
+  },
+  {
+    field: "price4",
+    component: "Input",
+    label: "设备净值(元):",
+    colProps: {
+      span: 10,
+    },
+    formItemProps: {
+      // rules: [required()],
+      labelWidth: "129px",
+      style: {
+        // width: "100px",
+      },
+    },
+    componentProps: {
+      placeholder: "请输入设备净值",
+      type: "number",
+      clearable: false,
+      // type: 'textarea',
+      inputStyle: {
+        width: "200px",
+      },
+    },
+  },
+  {
+    field: "price5",
+    component: "Input",
+    label: "发动机功率(kw):",
+    colProps: {
+      span: 10,
+    },
+    formItemProps: {
+      // rules: [required()],
+      labelWidth: "124px",
+      style: {
+        // width: "100px",
+      },
+    },
+    componentProps: {
+      placeholder: "请输入发动机功率",
+      type: "number",
+      clearable: false,
+      // type: 'textarea',
+      inputStyle: {
+        width: "200px",
+      },
+    },
+  },
+  {
+    field: "price6",
+    component: "Input",
+    label: "生成厂家:",
+    colProps: {
+      span: 10,
+    },
+    formItemProps: {
+      // rules: [required()],
+      labelWidth: "129px",
+      style: {
+        // width: "100px",
+      },
+    },
+    componentProps: {
+      placeholder: "请输入生产厂家",
+      type: "number",
+      clearable: false,
+      // type: 'textarea',
+      inputStyle: {
+        width: "200px",
+      },
+    },
+  },
+  {
+    field: "use_status",
+    label: "使用状态:",
+    component: "Radio",
+    colProps: {
+      span: 10,
+    },
+    formItemProps: {
+      // rules: [required()],
+      style: {
+        // width: "100px",
+      },
+    },
+    componentProps: {
+      options: [
+        {
+          label: "在用",
+          value: "1",
+        },
+        {
+          label: "闲置",
+          value: "2",
+        },
+      ],
+    },
+  },
+  {
+    field: "tec_status",
+    label: "技术状态:",
+    component: "Radio",
+    colProps: {
+      span: 10,
+    },
+    formItemProps: {
+      // rules: [required()],
+      labelWidth: "129px",
+      style: {
+        // width: "100px",
+      },
+    },
+    componentProps: {
+      options: [
+        {
+          label: "完好",
+          value: "1",
+        },
+        {
+          label: "待修/在修",
+          value: "2",
+        },
+        {
+          label: "待报废",
+          value: "3",
+        },
+      ],
+    },
+  },
+  {
+    field: "other_performance",
+    component: "Input",
+    label: "其它性能参数:",
+    colProps: {
+      span: 18,
+    },
+    formItemProps: {
+      // rules: [required()],
+      labelWidth: "129px",
+      style: {
+        // width: "100px",
+      },
+    },
+    componentProps: {
+      placeholder: "请输入性能参数",
+      clearable: false,
+      type: "textarea",
+      maxlength: "300",
+      showWordLimit: true,
+      rows: "6",
+      autoSize: { minRows: "2", maxRows: "8" },
+      // inputStyle: {
+      //   width: "200px",
+      // },
+    },
+  },
+  {
+    field: "upload",
+    colProps: {
+      span: 18,
+    },
+  },
+  {
+    field: "detial_info",
+    component: "Input",
+    label: "详细信息:",
+    colProps: {
+      span: 18,
+    },
+    formItemProps: {
+      // rules: [required()],
+      labelWidth: "129px",
+      style: {
+        // width: "100px",
+      },
+    },
+    componentProps: {
+      placeholder: "请输入详细信息",
+      clearable: false,
+      type: "textarea",
+      maxlength: "300",
+      showWordLimit: true,
+      rows: "6",
+      autoSize: { minRows: "2", maxRows: "8" },
+      // inputStyle: {
+      //   width: "200px",
+      // },
     },
   },
   {
@@ -131,23 +552,49 @@ const schema = reactive<FormSchema[]>([
   >
     <Form
       :schema="schema"
-      label-width="100px"
-      label-position="right"
+      label-width="126px"
+      label-position="top"
       @register="register"
     >
-      <template #submit>
-        <ElButton
-          :loading="loading"
-          type="primary"
-          class="w-[20%]"
-          @click="submitHandle"
+      <template #upload>
+        <ElUpload
+          v-model:file-list="fileList"
+          class="upload-file"
+          action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15"
+          :on-preview="handlePreview"
+          :on-remove="handleRemove"
+          list-type="picture"
         >
-          提交
-        </ElButton>
+          <ElButton type="primary">添加图片</ElButton>
+          <ElTooltip effect="dark" placement="right">
+            <template #content>
+              <div class="max-w-400px">
+                至少一张，最多八张。设备图片格式为jpg,png,gif。文件大小不超过5M，否则将不能上传。请从前后左右四个方向以及主要工作部件，内部结构等方面展示设备。建议图片的长边不低于800像素。
+              </div>
+            </template>
+            <Icon class="ml-5px" icon="bi:question-circle-fill" :size="14" />
+          </ElTooltip>
+          <template #tip>
+            <div class="el-upload__tip">
+              jpg/png files with a size less than 500kb
+            </div>
+          </template>
+        </ElUpload>
+      </template>
+      <template #submit>
+        <div class="submit-btn">
+          <ElButton
+            :loading="loading"
+            type="primary"
+            class="w-[20%]"
+            @click="submitHandle"
+          >
+            提交
+          </ElButton>
+        </div>
       </template>
     </Form>
-    <div>
-      <!-- <ElCascader
+    <!-- <ElCascader
         ref="cascaderRef"
         v-model="cascaderY"
         placeholder="省/市/区"
@@ -158,7 +605,6 @@ const schema = reactive<FormSchema[]>([
         clearable
         class="province-cascader"
       ></ElCascader> -->
-    </div>
   </ContentWrap>
 </template>
 
@@ -169,16 +615,23 @@ const schema = reactive<FormSchema[]>([
     background: #fff;
   }
   :deep(.province-cascader) {
-    width: 200px;
+    width: 222px;
   }
   :deep(.device-date-picker_custom) {
     // width: 300px;
     .el-input__wrapper {
-      width: 200px;
+      width: 222px;
     }
   }
   .title {
     color: @active-word-color;
+  }
+  .upload-file {
+    width: 400px;
+  }
+  .submit-btn {
+    display: flex;
+    justify-content: center;
   }
 }
 </style>
